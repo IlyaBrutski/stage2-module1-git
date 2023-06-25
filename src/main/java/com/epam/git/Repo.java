2163b0c1a0e14c61c7a05d36a3aefb6fd3aee88d
IlyaBrutski <101987935+IlyaBrutski@users.noreply.github.com>
2023-06-25 21:32:07 +0300

    Git_Task _2

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/IlyaBrutski/Git_Task.git";
+    public static String REPO_LINK = "https://github.com/IlyaBrutski/TaskGit.git";
 }
